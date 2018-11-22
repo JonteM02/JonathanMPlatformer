9435bd776a90aab47f17551b09c0551ba6e76a43
JonteM02 <jonathan.martensson@elev.ga.lbs.se>
2018-11-22 10:41:39 +0100

    First push

---
+++ Assets/Scripts/EnemyHofizontalMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHofizontalMovement : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    private bool isleft = true;
+    private Rigidbody2D rbody;
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        Move(false);
+    }
+
+    void Move(bool flip)
+    {
+        if (flip == true)
+        {
+            isleft = !isleft;
+        }
+        if (isleft == true)
+        {
+            rbody.velocity = new Vector2(-moveSpeed, rbody.velocity.y);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            rbody.velocity = new Vector2(moveSpeed, rbody.velocity.y);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "InvisibleWall")
+        {
+            Move(true);
+        }
+    }
+}
---
+++ Assets/Scripts/YouFailed.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouFailed : MonoBehaviour
+{
+    public string levelToLoad = "Scene 1";
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            print("You Failed");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
