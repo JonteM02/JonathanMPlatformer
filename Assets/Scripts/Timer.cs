5f8e55586b2dbcc554d4e76d35ce6910ac9e18da
JonteM02 <jonathan.martensson@elev.ga.lbs.se>
2018-12-10 08:00:37 +0100

    Added timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI TimerText;
+    public float StartTime;
+    // Use this for initialization
+    void Start()
+    {
+        StartTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = StartTime + Time.time;
+        //Visar på skärmen hur lång tid som gått
+        string minutes = ((int)t / 60).ToString();
+        //Visar hur många minuter som gått
+        string seconds = (t % 60).ToString("f2");
+        //Visar hur många sekunder som gått.
+        TimerText.text = minutes + ":" + seconds;
+    }
+
+}
